6644f980dacf78a52cf32dc54b3f36d1f0b779ac
Reinnovs <33583262+reinnovs@users.noreply.github.com>
2020-04-30 05:32:09 +0200

    website

---
+++ website/views.py
@@ -0,0 +1,17 @@
+
+from datetime import datetime
+from django.shortcuts import render
+from django.http import HttpResponse
+from meetings.models import Meeting
+
+
+# Create your views here.
+def welcome(request):
+    return render(request, "website/welcome.html",
+                  {"num_meetings": Meeting.objects.count()})
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("This is some text of the about page")
